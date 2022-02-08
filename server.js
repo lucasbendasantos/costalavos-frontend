c9577486687f31e43638649d1f9eebc0203cdfb3
Lucas PC <lucasbendasantos@hotmail.com>
2022-02-08 20:43:30 -0300

    Ajuste NODE

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + '/dist/_costalavos-frontend'))
+app.use(express.static(__dirname + '/dist/costalavos-frontend'))
 
 app.get('/*' , (req ,res) => {
-  res. sendFile(__dirname + '/dist/_costalavos-frontend/index.html')
+  res. sendFile(__dirname + '/dist/costalavos-frontend/index.html')
 });
 
 app.listen(PORT, () => {
